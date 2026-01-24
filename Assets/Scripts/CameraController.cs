a99d617d76f83d32b077b3fd69c1474749fae3b8
Nuttapon1547 <nuttaponkaewyoung1547@gmail.com>
2026-01-24 09:26:28 +0700

    map barrier

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+
+    [Header("Move")]
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] private Transform corner1;
+    [SerializeField] private Transform corner2;
+
+    [SerializeField] private float xInput;
+    [SerializeField] private float zInput;
+    public static CameraController instance;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        instance = this;
+        cam = Camera.main;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveSpeed = 50;
+        MoveByKB();
+    }
+
+    private void MoveByKB()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical");
+
+        Vector3 dir = (transform.forward * zInput) + (transform.right * xInput);
+
+        transform.position += dir * moveSpeed * Time.deltaTime;
+        transform.position = Clamp(corner1.position, corner2.position);
+    }
+
+    private Vector3 Clamp(Vector3 lowerLeft, Vector3 topRight)
+    {
+        Vector3 pos = new Vector3(Mathf.Clamp(transform.position.x, lowerLeft.x, topRight.x),
+                                  transform.position.y,
+                                  Mathf.Clamp(transform.position.z, lowerLeft.z, topRight.z));
+
+        return pos;
+    }
+}
